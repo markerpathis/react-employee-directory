9ed684b39558ba40f7c8d9ec31baf43e36976027
Parker Mathis <parkerjmathis@gmail.com>
2023-04-23 13:51:04 -0700

    Added moment to reformat utc dob values

--- src/components/DirectoryTable.js
+++ src/components/DirectoryTable.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
+import moment from "moment";
 
 function DirectoryTable(props) {
   const [users, setUsers] = useState([]);
@@ -19,6 +20,25 @@ function DirectoryTable(props) {
     getRandomUsers();
   }, []);
 
+  const populateUserData = () => {
+    return users.map((user, index) => {
+      const dobFormatted = moment.utc(user.dob.date).format("MM-DD-YYYY");
+      return (
+        <tr key={index}>
+          <td>
+            <img src={user.picture.thumbnail} />
+          </td>
+          <td>
+            {user.name.first} {user.name.last}
+          </td>
+          <td>{user.phone}</td>
+          <td>{user.email}</td>
+          <td>{dobFormatted}</td>
+        </tr>
+      );
+    });
+  };
+
   return (
     <Container>
       <Table striped bordered hover>
@@ -32,21 +52,7 @@ function DirectoryTable(props) {
           </tr>
         </thead>
         {users ? (
-          <tbody>
-            {users.map((user, index) => (
-              <tr key={index}>
-                <td>
-                  <img src={user.picture.thumbnail} />
-                </td>
-                <td>
-                  {user.name.first} {user.name.last}
-                </td>
-                <td>{user.phone}</td>
-                <td>{user.email}</td>
-                <td>{user.dob.date}</td>
-              </tr>
-            ))}
-          </tbody>
+          <tbody>{populateUserData()}</tbody>
         ) : (
           <tbody>
             <tr>
